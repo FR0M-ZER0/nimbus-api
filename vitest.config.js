464448e095ef03d9f54f9f28a95d36f323fa5796
gabrielbguimaraes <biel_google@hotmail.com>
2025-10-28 18:53:05 -0300

    fix(1.1): ajustando vitest config2

--- vitest.config.js
+++ vitest.config.js
@@ -2,23 +2,20 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+
     environment: 'node',
+
+
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
       '**/src/generated/prisma/**',
     ],
-  },
 
-  build: {
-    sourcemap: 'inline', 
-    rollupOptions: {
-      output: {
-        sourcemapIgnoreList: (relativeSourcePath, sourcemapPath) => {
-          return relativeSourcePath.includes('src/generated/prisma');
-        },
-      },
+
+    logHeapUsage: false, 
+    sourcemapIgnoreList: (sourcePath, _mapPath) => {
+      return sourcePath.includes('src/generated/prisma');
     },
   },
-
 });
